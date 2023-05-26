c01f9bff28335fc8a7a23d4d65bc2ff4df3a0460
KarolKosholko <133325176+KarolKosholko@users.noreply.github.com>
2023-05-26 09:51:34 +0300

    Add files via upload

---
+++ lab3/src/lab3.java
@@ -0,0 +1,10 @@
+public class lab3 {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            int number = 10;
+            System.out.print("Додатні дільники числа " + number + ": ");
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0 && i > 0) {
+                    System.out.print(i + " ");
+                }
+
---
+++ lab3/src/lab5.java
@@ -0,0 +1,20 @@
+public class lab5 {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            int n = 10;
+            int factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("Факторіал числа " + n + " дорівнює " + factorial);
+
+            int m = 10;
+            int factor = 1;
+            int o = 1;
+            while (o <= n) {
+                factor *= o;
+                o++;
+            }
+            System.out.println("Факторіал числа " + m + " дорівнює " + factor);
+        }
+
