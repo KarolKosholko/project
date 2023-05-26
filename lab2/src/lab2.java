0cd6fb86ce6890cf33fe3d5dadc3550530313b62
KarolKosholko <133325176+KarolKosholko@users.noreply.github.com>
2023-05-26 09:46:04 +0300

    Add files via upload

---
+++ lab2/src/lab2.java
@@ -0,0 +1,9 @@
+public class lab2 {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            int n = 21;
+            if ((n > 25) && (n < 100)) {
+                System.out.println("Число " + n + "в проміжку (25;100)");
+            } else {
+                System.out.println("Число " + n + " не в проміжку (25;100)");
+            }
