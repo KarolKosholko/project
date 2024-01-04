490f7a01121120f6cc438280f62335c6453c9be6
KarolKosholko <133325176+KarolKosholko@users.noreply.github.com>
2024-01-04 20:56:52 +0200

    Delete lab5.16 directory

--- lab5.16/src/lab3.java
+++
@@ -1,9 +0,0 @@
-public class lab3 {
-    public static void main(String[] args) {
-        int randomNum = (int)(Math.random() * 101);
-        System.out.println("The randomly generated number: " + randomNum);
-
-        String num = Integer.toString(randomNum);
-        System.out.println("The quantity of digits in the nuber: " + num.length() );
-    }
-    }
