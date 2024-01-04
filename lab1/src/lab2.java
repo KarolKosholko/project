9394391a227f47533ef36f8488882993fdb3c75d
KarolKosholko <133325176+KarolKosholko@users.noreply.github.com>
2024-01-04 20:51:46 +0200

    Delete lab1 directory

--- lab1/src/lab2.java
+++
@@ -1,7 +0,0 @@
-public class lab2 {
-    public static void main(String[] args) {
-        int num = 56;
-        int sum = (num / 10) + (num % 10);
-        System.out.println("Сума" + num + " =" + sum);
-    }
-}
